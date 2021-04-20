b28ba3770100b970b96f81b2fee670849cd9f0ab
NeKoSaNnn <nekomjx@qq.com>
2021-04-20 18:46:42 +0800

    update database

--- Album/models.py
+++ Album/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.core import validators
 from django.utils import timezone
 from django.contrib import admin
 import datetime
 
 
 class User(models.Model):
-    name=models.CharField(max_length=200,default='')
-    pwd=models.CharField(max_length=200,default='')
-    now_capacity=models.FloatField(default=0)
+    phone = models.CharField(max_length=11, default="",
+                             validators=[validators.RegexValidator("1[345678]\d{9}", message="请输入正确格式的手机号")],
+                             verbose_name="手机号", primary_key=True)
+    name = models.CharField(max_length=200, default='',
+                            validators=[validators.MinLengthValidator(0, message="用户昵称不可为空")], verbose_name="昵称")
+
+    pwd = models.CharField(max_length=200, default='',
+                           validators=[validators.MinLengthValidator(0, message="用户密码不可为空")], verbose_name="密码")
+    now_capacity = models.FloatField(default=0)
     max_capacity = models.FloatField(default=0)
 
     def __str__(self):
@@ -15,8 +22,8 @@ class User(models.Model):
 
 
 class Face(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    face_cover = models.CharField(max_length=200,default='')
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    face_cover = models.CharField(max_length=200, default='')
     cnt = models.IntegerField(default=0)
 
     def __str__(self):
@@ -24,19 +31,19 @@ class Face(models.Model):
 
 
 class Video(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    name=models.CharField(max_length=200,default='')
-    create_time=models.DateTimeField()
-    time=models.TimeField()
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default='')
+    create_time = models.DateTimeField()
+    time = models.TimeField()
     description = models.CharField(max_length=1000, default='')
-    size=models.FloatField(default=0)
+    size = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
 
 
 class Folder(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
     name = models.CharField(max_length=200, default='')
     cnt = models.IntegerField(default=0)
     total_size = models.FloatField(default=0)
@@ -48,16 +55,16 @@ class Folder(models.Model):
 
 
 class Tag(models.Model):
-    tag=models.CharField(max_length=200, default='')
+    tag = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.tag
 
 
 class Picture(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
+    folder = models.ForeignKey("Folder", on_delete=models.CASCADE)
     name = models.CharField(max_length=200, default='')
     type = models.CharField(max_length=200, default='')
     upload_time = models.DateTimeField()
@@ -66,7 +73,7 @@ class Picture(models.Model):
     size = models.FloatField(default=0)
     height = models.IntegerField(default=0)
     width = models.IntegerField(default=0)
-    is_tag=models.BooleanField(default=False)
+    is_tag = models.BooleanField(default=False)
     is_face = models.BooleanField(default=False)
 
     def __str__(self):
@@ -74,33 +81,33 @@ class Picture(models.Model):
 
 
 class UserTag(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
     cnt = models.IntegerField(default=0)
 
     class Meta:
-        unique_together=['user','tag']
+        unique_together = ['user', 'tag']
 
 
 class TagCover(models.Model):
-    pic = models.ForeignKey(Picture, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    pic = models.ForeignKey("Picture", on_delete=models.CASCADE)
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
 
     class Meta:
-        unique_together=['pic','tag']
+        unique_together = ['pic', 'tag']
 
 
 class FolderCover(models.Model):
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
-    pic = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    folder = models.ForeignKey("Folder", on_delete=models.CASCADE)
+    pic = models.ForeignKey("Picture", on_delete=models.CASCADE)
 
     class Meta:
-        unique_together=['folder','pic']
+        unique_together = ['folder', 'pic']
 
 
 class FacePic(models.Model):
-    pic = models.ForeignKey(Picture, on_delete=models.CASCADE)
-    face = models.ForeignKey(Face, on_delete=models.CASCADE)
+    pic = models.ForeignKey("Picture", on_delete=models.CASCADE)
+    face = models.ForeignKey("Face", on_delete=models.CASCADE)
 
     class Meta:
-        unique_together=['pic','face']
+        unique_together = ['pic', 'face']
