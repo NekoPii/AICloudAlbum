e802008a3aabd53a629168de3ae3ed25588a051c
NeKoSaNnn <nekomjx@qq.com>
2021-04-22 23:58:54 +0800

    4-22 update animate

--- Album/static/js/changebackground.js
+++ Album/static/js/changebackground.js
@@ -1,13 +1,14 @@
 
 var arrImg = ['/static/image/welcome1.jpg', '/static/image/welcome2.jpg', '/static/image/welcome3.jpg','/static/image/welcome4.jpg'];
 var len = arrImg.length;
-var index=0;
-
+var index=1;
 
 $(document).ready(function () {
+        $("#alpha").css("backgroundImage","url("+arrImg[0]+")");
 
         setInterval(function () {
             $("#alpha").css("backgroundImage","url("+arrImg[(index++)%len]+")");
+
 
         }, 10000);
 
