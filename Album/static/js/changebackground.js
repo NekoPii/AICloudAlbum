8fb2b3e3a7c5039c4fd1d0979e94e06a1ecd64d5
NeKoSaNnn <nekomjx@qq.com>
2021-04-24 15:07:29 +0800

    compress image

--- Album/static/js/changebackground.js
+++ Album/static/js/changebackground.js
@@ -1,16 +1,13 @@
-
-var arrImg = ['/static/image/welcome1.jpg', '/static/image/welcome2.jpg', '/static/image/welcome3.jpg','/static/image/welcome4.jpg'];
+var arrImg = ['/static/image/welcome0.jpg', '/static/image/welcome1.jpg', '/static/image/welcome2.jpg', '/static/image/welcome3.jpg', '/static/image/welcome4.jpg'];
 var len = arrImg.length;
-var index=1;
+var index = 1;
 
 $(document).ready(function () {
-        $("#alpha").css("backgroundImage","url("+arrImg[0]+")");
+    $("#alpha").css("backgroundImage", "url(" + arrImg[0] + ")");
 
-        setInterval(function () {
-            $("#alpha").css("backgroundImage","url("+arrImg[(index++)%len]+")");
-
-
-        }, 10000);
+    setInterval(function () {
+        $("#alpha").css("backgroundImage", "url(" + arrImg[(index++) % len] + ")");
+    }, 10000);
 
 });
 
