c2d51664b8429af6a9b8ba58ee7fff8fb7871d2d
LabmemNo004 <1085201315@qq.com>
2021-05-02 18:07:07 +0800

    update sidebar /navbar

--- Album/static/js/navbar.js
+++ Album/static/js/navbar.js
@@ -5,36 +5,73 @@
 */
 
 
-(function($) {
+(function ($) {
     "use strict";
 
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function () {
+        if ($(".navbar").offset().top > 20) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
     });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $('#search_1').bind('keyup', function (event) {
+
+        if (event.keyCode == "13") {
+
+            //回车执行查询
+
+            console.log('click');
+            ;
+
+        }
+
+    });
 
     // closes the responsive menu on menu item click
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
+    $(".navbar-nav li a").on("click", function (event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
     });
 
+    $('.item.sidebar-button').on("click", function (event) {
+
+        $('.ui.sidebar').sidebar({
+                context: 'body',
+                //dimPage: false,
+                scrollLock:true,
+                onVisible: function () {
+                    $('#alpha').addClass('pushable');
+                },
+                onShow: function () {
+                    //$('#alpha').removeClass('pushable');
+                },
+                onHide:function (){
+                    $('#alpha').addClass('pushable');
+                },
+                onHidden: function () {
+                    $('#alpha').removeClass('pushable');
+                }
+            }
+        )
+            .sidebar('toggle');
+
+        // $('#alpha').removeClass('pushable');
+    });
 
 
 })(jQuery);
