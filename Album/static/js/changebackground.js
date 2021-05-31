e0454d59ff250f4bb4b3f55d9d2a3bc46615350a
LabmemNo004 <1085201315@qq.com>
2021-05-31 09:24:59 +0800

    fix navbar

--- Album/static/js/changebackground.js
+++ Album/static/js/changebackground.js
@@ -1,26 +1,39 @@
 var len = 5;
 var index = 1;
-var imgs=[]
-var basic_url='/static/image/welcome'
+var imgs = []
+var basic_url = '/static/image/welcome'
 $(document).ready(function () {
-    var now_img=new Image();
-    now_img.src=basic_url+'0'+'.jpg';
-    var now_img1=new Image();
-    now_img1.src=basic_url+'1'+'.jpg';
+    var now_img = new Image();
+    now_img.src = basic_url + '0' + '.jpg';
+    var now_img1 = new Image();
+    now_img1.src = basic_url + '1' + '.jpg';
     imgs.push(now_img);
     imgs.push(now_img1);
 
-    $("#alpha").css("backgroundImage","url("+imgs[0].src+")");
+    $("#alpha").css("backgroundImage", "url(" + imgs[0].src + ")");
 
     setInterval(function () {
-        $("#alpha").css("backgroundImage",  "url("+imgs[(index++)%len].src+")");
-        if(index<=len){
-            var now_img=new Image();
-            now_img.src=basic_url+index.toString()+'.jpg';
+        $("#alpha").css("backgroundImage", "url(" + imgs[(index++) % len].src + ")");
+        if (index <= len) {
+            var now_img = new Image();
+            now_img.src = basic_url + index.toString() + '.jpg';
             imgs.push(now_img);
         }
 
     }, 6000);
+
+    /* Rotating Text - Morphtext */
+    $("#js-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "fadeIn",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 2000,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
 
 });
 
