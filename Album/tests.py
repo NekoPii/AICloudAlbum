dd70d776751c8128ca37dd31fc3d37cebc58d54f
LabmemNo004 <1085201315@qq.com>
2021-06-13 14:55:19 +0800

    add tags into test database

--- Album/tests.py
+++ Album/tests.py
@@ -1,6 +1,42 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from . import models
+# Create your tests here.
+
+# testing delete_select_image module
+
+class TestDeleteSelectImageOrigin(TestCase):
+    def setUp(self):
+        print('setUp')
+
+    def tearDown(self):
+        print('tearDown')
+
+    def test_demo(self):
+        models.Tag(tag="None").save()
+        file=open("/Users/tanzhongyu/AICloudAlbum/AI/ImgClass_new/config/tags.txt")
+        line=file.readline()
+        while line:
+            models.Tag(tag=line).save()
+            line = file.readline()
+
+        print('test_demo')
+
+    def test_welcome(self):
+        response=self.client.get("/welcome/")
+
+        print('test_demo2')
+
+
+
+
+
+
+# testing Face Detect module
+
+# testing delete module
 
 # testing delete module
 
