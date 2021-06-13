d37577bf3a1aac79b6b1bd46c175758b4d878b41
hyo6191110 <hyo619@tongji.edu.cn>
2021-06-13 14:59:09 +0800

    Update tests.py

--- Album/tests.py
+++ Album/tests.py
@@ -30,14 +30,40 @@ class TestDeleteSelectImageOrigin(TestCase):
         print('test_demo2')
 
 
+# testing face module
+from AI.FaceDetect.FaceDetect import FaceRecogPrepared
+from AI.FaceDetect.VisualizeDetect import VisualizeBlocks
+import Album.models as models
+import os
 
+imgs_dir = os.getcwd()
 
+class FaceDetectTest(TestCase):
+    # 测试人脸识别FaceDetect模块
+    def setUp(self):
+        # 测试函数执行前执行
+        pass
 
+    def test_post_user(self):
+        # 创建用户
+        pass
 
-# testing Face Detect module
+    def test_face1(self):
+        self.maxDiff=None
+        post_signup_data={
+            'phone': '15989061915',
+            'name': 'hhhhh',
+            'pwd': '123456',
+            're_pwd': '123456'
+        }
+        post_login_data = {
+            'phone': '15989061915',
+            'pwd': '123456'
+        }
+        response = self.client.post('/signup', data=post_signup_data,
+                                    content_type='application/json')
+        self.assertEqual(self.client.session['signup'], True)
 
-# testing delete module
-
-# testing delete module
-
-# testing Face Detect module
+        def tearDown(self):
+            # 每个测试函数执行后都删除所有数据
+            models.User.objects.all().delete()
