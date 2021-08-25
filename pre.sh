3f3e294f7b419287d62914961ed86bbd365cc012
mjx <1427466423@qq.com>
2021-08-26 00:54:00 +0800

    update

--- pre.sh
+++ pre.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+rm -r static
 python manage.py compress
 mkdir collect_static
 yes yes | python manage.py collectstatic
-pip install -r requirements.txt
+pipreqs ./ --encoding=utf-8 --force
+#pip install -r requirements.txt
