6d36555eb69d7b1409fab64dc566dbaef1fba1a5
夜鹿 <maojingxin.mjx@alibaba-inc.com>
2021-08-27 14:17:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,23 @@ apt-get install -y sudo
 
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install -y python3.8
+sudo apt-get install -y pip
 sudo apt-get install -y wget
 sudo apt-get install -y build-essential cmake
 sudo apt-get install -y gcc-6 g++-6
 sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y libboost-all-dev
 sudo apt-get install -y libopenblas-dev liblapack-dev libatlas-base-dev libblas-dev gfortran
-sudo apt-get install libhdf5-serial-dev
+sudo apt-get install -y libhdf5-serial-dev
 sudo apt-get install -y openssl
-sudo apt-get install -y python3.8
-sudo apt-get install -y pip
-udo  apt-get install -y python-pip python-setuptools python-dev
 sudo apt-get install -y ffmpeg
 
-mkdir ~/download
-cd ~/download
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-aarch64.sh
-sh Anaconda3-2021.05-Linux-aarch64.sh
-rm Ana*.sh
+#mkdir ~/download
+#cd ~/download
+#wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-aarch64.sh
+#sh -y Anaconda3-2021.05-Linux-aarch64.sh
+#rm Ana*.sh
 
 sudo pip install --upgrade pip
 sudo pip install Cython
