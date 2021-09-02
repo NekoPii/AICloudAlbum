c1476433e299ddd5cd28642115bc9eacbc9ab80f
夜鹿 <maojingxin.mjx@alibaba-inc.com>
2021-09-02 14:18:33 +0800

    deploy

--- Dockerfile
+++ Dockerfile
@@ -9,13 +9,16 @@ RUN mkdir -p /AICloudAlbum
 WORKDIR /AICloudAlbum
 ADD . /AICloudAlbum
 RUN pip install --upgrade pip
+##RUN if [ ! -f "tensorflow-2.4.0-cp37-none-linux_aarch64.whl" ];  \
+##    then wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_aarch64.whl; \
+##    fi
+#RUN pip install tensorflow-2.4.0-cp37-none-linux_aarch64.whl
 RUN pip install -r /AICloudAlbum/requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
-RUN wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_aarch64.whl
-RUN pip install tensorflow-2.4.0-cp37-none-linux_aarch64.whl
 RUN rm -r static
 RUN yes yes | python manage.py compress
 RUN mkdir -p collect_static
 RUN yes yes | python manage.py collectstatic
-EXPOSE 80 8000 9191
-RUN supervisord
+RUN echo_supervisord_conf > /etc/supervisord.conf
+RUN supervisord -c /AICloudAlbum/supervisord.conf
 RUN supervisorctl start daphne
+EXPOSE 80 8000 9192
--- deploy.sh
+++ deploy.sh
@@ -1,60 +1,58 @@
 #!/bin/sh
+python="python3.8"
+pip_source="https://mirrors.aliyun.com/pypi/simple/"
+nginx_Download="http://nginx.org/download/nginx-1.19.7.tar.gz"
+nginx="nginx-1.19.7"
+
 apt-get update
 apt-get upgrade
-apt-get install -y sudo
-apt-get install -y vim
+apt-get install -y sudo vim
 sudo cp -f ./sources.list /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get install -y python3.8
+sudo apt-get install -y $python
 sudo apt-get install -y pip
-sudo pip install --upgrade pip
+sudo pip install --upgrade pip -i $pip_source
 sudo apt-get install -y build-essential cmake
-sudo apt-get install -y libgtk-3-dev
-sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libgtk-3-dev libboost-all-dev
 sudo apt-get install -y libopenblas-dev liblapack-dev libatlas-base-dev libblas-dev gfortran
-sudo apt-get install -y libhdf5-serial-dev
-sudo apt-get install -y openssl
-sudo apt-get install -y ffmpeg
-sudo apt-get install -y wget
-
-mkdir ~/download
-cd ~/download
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-aarch64.sh
-sh Anaconda3-2021.05-Linux-aarch64.sh
-rm Ana*.sh
+sudo apt-get install -y libhdf5-serial-dev openssl libssl-dev libpcre3 libpcre3-dev zlib1g-dev
+sudo apt-get install -y ffmpeg wget
 
 sudo pip install Cython
 #sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.6.0-cp38-cp38-manylinux2010_x86_64.whl
 sudo pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 
-mkdir ~/AICloudAlbum
-cp -r . ~/AICloudAlbum
-cd ~/AICloudAlbum
+cd /
+mkdir -p /AICloudAlbum
+cp -r . /AICloudAlbum
+cd /AICloudAlbum
 
 rm -r static
-yes yes | python3.8 manage.py compress
-mkdir collect_static
-yes yes | python3.8 manage.py collectstatic
+yes yes | $python manage.py compress
+mkdir -p collect_static
+yes yes | $python manage.py collectstatic
 
+
+# Build Nginx
 cd /usr/local
 mkdir nginx
 cd /usr/local/nginx
 rm nginx-*.tar.gz
-wget http://nginx.org/download/nginx-1.19.7.tar.gz
-tar -zxvf nginx-1.19.7.tar.gz
-cd /usr/local/nginx/nginx-1.19.7
-./configure --prefix=/usr/local/nginx/nginx-1.19.7 --with-http_ssl_module
-make install
+wget $nginx_Download
+tar -zxvf "$nginx".tar.gz
+cd /usr/local/nginx/$nginx
+./configure --prefix=/usr/local/nginx --with-http_ssl_module
+make && make install
 cd /usr/local/nginx/conf
-mkdir cert
+mkdir -p cert
 mv -f ./nginx/cert /usr/local/nginx/conf/cert/
 mv -f ./nginx/nginx.conf /usr/local/nginx/conf/nginx.conf
 cd /usr/local/nginx/sbin
+./nginx
 ./nginx -s reload
 
-supervisord
+# Build Supervisor
+supervisord -c supervisord.conf
 supervisorctl start daphne
